c115a69beebbaed98b558f7504df788dd1d21e9d
LAGARDE Cyril <cy.lagarde@gmail.com>
2018-11-29 20:36:29 +0100

    define StyledToolTip with control

--- cl.plugin.consistency/src/cl/plugin/consistency/tooltip/StyledToolTip.java
+++ cl.plugin.consistency/src/cl/plugin/consistency/tooltip/StyledToolTip.java
@@ -139,7 +139,8 @@ public class StyledToolTip extends ToolTip
     };
   }
 
-  Set<ImageStyler> imageStylers = new HashSet<>();
+  final Set<ImageStyler> imageStylers = new HashSet<>();
+  final Set<ControlStyler> controlStylers = new HashSet<>();
 
   @Override
   protected StyledText createToolTipContentArea(Event event, Composite parent)
@@ -161,25 +162,75 @@ public class StyledToolTip extends ToolTip
         imageStyler.start = styleRange.start;
         imageStylers.add(imageStyler);
       }
+      else if (styleRange.data instanceof ControlStyler)
+      {
+        if (styleRange.length != 1)
+          throw new RuntimeException("Cannot create " + getClass().getSimpleName() + ": text length for control != 1");
+        ControlStyler controlStyler = (ControlStyler) styleRange.data;
+
+        // create control
+        controlStyler.control = controlStyler.controlCreator.apply(styledText);
+
+        // change size
+        Point size = controlStyler.control.getSize();
+        if (size.x == 0 || size.y == 0)
+        {
+          controlStyler.control.pack();
+          size = controlStyler.control.getSize();
+        }
+        controlStyler.start = styleRange.start;
+        controlStylers.add(controlStyler);
+
+        // change style.metrics
+        for(TextStyle style : controlStyler.textStyles)
+          style.metrics = new GlyphMetrics(Math.max(0, size.y + controlStyler.y_offset), Math.max(0, -controlStyler.y_offset), size.x);
+        controlStyler.textStyles.clear();
+      }
     }
 
     styledText.setText(styledString.getString());
     styledText.setStyleRanges(styleRanges);
 
-    if (!imageStylers.isEmpty())
+    if (!imageStylers.isEmpty() || !controlStylers.isEmpty())
     {
       styledText.addPaintObjectListener(e -> {
         GC gc = e.gc;
         StyleRange style = e.style;
         int start = style.start;
+
         for(ImageStyler imageStyler : imageStylers)
         {
           if (start == imageStyler.start)
           {
             int x = e.x;
-            int y = e.y + e.ascent - style.metrics.ascent;
+            int y = e.y + e.ascent;
+
+            if (style.metrics.descent == 0)
+              y -= style.metrics.ascent;
+            else
+              y += style.metrics.descent - imageStyler.image.getBounds().height;
+
             gc.drawImage(imageStyler.image, x, y);
-            break;
+
+            return;
+          }
+        }
+
+        for(ControlStyler controlStyler : controlStylers)
+        {
+          if (start == controlStyler.start)
+          {
+            int x = e.x;
+            int y = e.y + e.ascent;
+
+            if (style.metrics.descent == 0)
+              y -= style.metrics.ascent;
+            else
+              y += style.metrics.descent - controlStyler.control.getSize().y;
+
+            controlStyler.control.setLocation(x, y);
+
+            return;
           }
         }
       });
@@ -219,7 +270,37 @@ public class StyledToolTip extends ToolTip
     {
       style.data = this;
       Rectangle rect = image.getBounds();
-      style.metrics = new GlyphMetrics(rect.height - y_offset, y_offset, rect.width);
+      style.metrics = new GlyphMetrics(Math.max(0, rect.height + y_offset), Math.max(0, -y_offset), rect.width);
+    }
+  }
+
+  /**
+   * The class <b>ControlStyler</b> allows to.<br>
+   */
+  public static class ControlStyler extends Styler
+  {
+    final Function<Composite, Control> controlCreator;
+    final int y_offset;
+    int start;
+    Control control;
+    Set<TextStyle> textStyles = new HashSet<>();
+
+    public ControlStyler(Function<Composite, Control> controlCreator, int y_offset)
+    {
+      this.controlCreator = controlCreator;
+      this.y_offset = y_offset;
+    }
+
+    public ControlStyler(Function<Composite, Control> controlCreator)
+    {
+      this(controlCreator, 0);
+    }
+
+    @Override
+    public void applyStyles(TextStyle style)
+    {
+      style.data = this;
+      textStyles.add(style);
     }
   }
 }
