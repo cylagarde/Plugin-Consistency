52964aff877905415e8eb40345fb58465bcf61d3
LAGARDE Cyril <cy.lagarde@gmail.com>
2019-06-16 06:39:06 +0200

    use tycho version 1.4.0

--- change_version.bat
+++ change_version.bat
@@ -1,3 +1,4 @@
 cd /d %0\..
+@findstr /C:SNAPSHOT  pom.xml
 set /p version="enter version X.Y.Z "
 mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=%version%-SNAPSHOT
